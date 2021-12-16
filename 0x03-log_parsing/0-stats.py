2b1f5876fde13273276d3c911f5ddcb6c8eb5ff5
alazar-des <alazar.des@gmail.com>
2021-12-16 18:22:36 +0300

    split fix

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -30,8 +30,8 @@ if __name__ == "__main__":
         for line in sys.stdin:
             count += 1
             try:
-                status_code = line.split('"')[2].split(" ")[1]
-                file_size = line.split('"')[2].split(" ")[2]
+                status_code = line.split(" ")[-2]
+                file_size = line.split(" ")[-1]
                 if status_code in info["status_code"].keys():
                     info["status_code"][status_code] += 1
                     info["file_size"] += int(file_size)
