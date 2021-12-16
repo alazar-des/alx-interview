71b514313953edeb40ff1b4bb237a065549630b2
alazar-des <alazar.des@gmail.com>
2021-12-16 18:08:58 +0300

    remove regex

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 """ Log parsing. """
-import re
 import sys
 
 
@@ -27,24 +26,21 @@ if __name__ == "__main__":
             "500": 0
         }
     }
-    rgx = re.compile(r'^((25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])\.)'
-                     r'{3}(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])\s'
-                     r'-'
-                     r'\s\[\d{4}\-(0?[1-9]|1[012])\-(0?[1-9]|[12][0-9]|3[01])'
-                     r'\s\d{2}:\d{2}:\d{2}\.\d+\]'
-                     r'\s\"GET\s\/projects\/260\sHTTP\/1.1\"\s\d+\s\d+$')
     try:
         for line in sys.stdin:
             count += 1
-            if re.match(rgx, line):
+            try:
                 status_code = line.split('"')[2].split(" ")[1]
                 file_size = line.split('"')[2].split(" ")[2]
                 if status_code in info["status_code"].keys():
                     info["status_code"][status_code] += 1
                     info["file_size"] += int(file_size)
+            except Exception:
+                pass
             count %= 10
             if count == 0:
                 printStat(info)
     except KeyboardInterrupt:
         printStat(info)
         raise
+    printStat(info)
