b09602edfbde4c44d65197f43671d48a51e061d8
alazar-des <alazar.des@gmail.com>
2022-01-27 19:29:28 +0300

    make change

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""Make a change module
+"""
+
+
+def makeChange(coins, total):
+    """return the smalles combination of possible changes
+    """
+    coins.sort(reverse=True)
+    while coins:
+        coin = 0
+        i = 0
+        while total != 0:
+            if total < coins[-1] or i == len(coins):
+                break
+            div = int(total / coins[i])
+            mod = total % coins[i]
+            if div > 0:
+                coin += div
+                total = mod
+            i += 1
+        if total == 0:
+            return coin
+        coins.pop(0)
+    return -1
