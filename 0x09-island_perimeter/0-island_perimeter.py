737823ee5e0433cc55622e6a3a3135b4a969ce4a
alazar-des <alazar.des@gmail.com>
2022-02-03 10:42:54 +0300

    edge fix

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -7,15 +7,15 @@ def island_perimeter(grid):
     """if 1 check its sides and if 0 add 1 to perimeter.
     """
     perimeter = 0
-    for i in range(1, len(grid) - 1):
-        for j in range(1, len(grid[0]) - 1):
+    for i in range(0, len(grid)):
+        for j in range(0, len(grid[0])):
             if (grid[i][j] == 1):
-                if grid[i - 1][j] == 0:
+                if i - 1 < 0 or grid[i - 1][j] == 0:
                     perimeter += 1
-                if grid[i + 1][j] == 0:
+                if i + 1 >= len(grid) or grid[i + 1][j] == 0:
                     perimeter += 1
-                if grid[i][j - 1] == 0:
+                if j - 1 < 0 or grid[i][j - 1] == 0:
                     perimeter += 1
-                if grid[i][j + 1] == 0:
+                if j + 1 >= len(grid[i]) or grid[i][j + 1] == 0:
                     perimeter += 1
     return perimeter
