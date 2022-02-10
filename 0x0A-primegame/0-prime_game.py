087d570fcdc79ab9a3e6547c7b54719243c5e8d0
alazar-des <alazar.des@gmail.com>
2022-02-10 10:24:21 +0300

    prime game

---
+++ 0x0A-primegame/0-prime_game.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""Prime Game
+"""
+
+
+def sieveOfEratosthenes(n):
+    """return prime numbers upto n"""
+    primes = [True] * (n + 1)
+    p = 2
+    while (p * p <= n):
+        if primes[p]:
+            for i in range(p * p, n + 1, p):
+                primes[i] = False
+        p += 1
+    return [p for p in range(2, n + 1) if primes[p] is True]
+
+
+def isWinner(x, nums):
+    """return name of the player that won the most round"""
+    if x == 0:
+        return None
+    countMaria = 0
+    countBen = 0
+    for n in nums:
+        primes = sieveOfEratosthenes(n)
+        if len(primes) % 2 != 0:
+            countMaria += 1
+        else:
+            countBen += 1
+    if countMaria > countBen:
+        return "Maria"
+    elif countBen > countMaria:
+        return "Ben"
+    else:
+        return None
